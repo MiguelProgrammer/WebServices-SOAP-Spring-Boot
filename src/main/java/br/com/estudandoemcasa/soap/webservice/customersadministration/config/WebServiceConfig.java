c42027abd9c402e19f3fa4113738795ac9c04799
miguelProgrammer <miguel.silva@mackenzista.com.br>
2023-09-09 11:37:17 -0300

    WebServiceConfig Gerando WSDL

--- src/main/java/br/com/estudandoemcasa/soap/webservice/customersadministration/config/WebServiceConfig.java
+++ src/main/java/br/com/estudandoemcasa/soap/webservice/customersadministration/config/WebServiceConfig.java
@@ -4,10 +4,12 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
-
-import javax.servlet.ServletRegistration;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 @Configuration
 @EnableWs
@@ -19,6 +21,23 @@ public class WebServiceConfig {
 
         dispatcherServlet.setApplicationContext(context);
         dispatcherServlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean(dispatcherServlet,"ws/*");
+        return new ServletRegistrationBean(dispatcherServlet,"/ws/*");
+    }
+
+    @Bean
+    public XsdSchema customerSchema(){
+        return new SimpleXsdSchema(new ClassPathResource("costumer-information.xsd"));
+    }
+
+    @Bean(name = "customers")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema schema){
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+
+        wsdl11Definition.setPortTypeName("CustomerPort");
+        wsdl11Definition.setTargetNamespace("http://miguelprogrammer.com.br");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setSchema(schema);
+
+        return wsdl11Definition;
     }
 }
