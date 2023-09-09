8f98a642efa488d6967401d0147be3afea95f9ca
miguelProgrammer <miguel.silva@mackenzista.com.br>
2023-09-09 09:21:36 -0300

    Create Project WebService

---
+++ src/main/java/br/com/estudandoemcasa/soap/webservice/customersadministration/CustomersAdministrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudandoemcasa.soap.webservice.customersadministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersAdministrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudandoemcasa/soap/webservice/customersadministration/CustomersAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudandoemcasa.soap.webservice.customersadministration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
